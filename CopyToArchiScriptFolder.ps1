699c9ea0e1019f00874d720a7bc2107a7ae564ad
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2021-10-07 20:48:04 +0200

    made the scripts more modular

--- CopyToArchiScriptFolder.ps1
+++ CopyToArchiScriptFolder.ps1
@@ -1,1 +1,3 @@
-Copy-Item "SetVisualAppereance.ajs" -Destination "$env:APPDATA\Archi4\scripts"
+Copy-Item "SetVisualAppereance.ajs" -Destination "$env:APPDATA\Archi4\scripts" -Force
+Copy-Item "lib\" -Destination "$env:APPDATA\Archi4\scripts" -Force -Recurse
+Copy-Item "util\" -Destination "$env:APPDATA\Archi4\scripts" -Force -Recurse
